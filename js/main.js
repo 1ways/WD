d5d82d2f74807257cc587f1dc9c3ddf381b1e32f
Anton <grutoha@gmail.com>
2021-05-06 17:28:02 +0300

    asdasd

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ $(function(){
     const burgerMenu = document.querySelector(".menu__icon");
     if (burgerMenu) {
         const responsiveMenu = document.querySelector(".menu__body");
-        burgerMenu.addEventListener("click", function (e){
+        burgerMenu.addEventListener("click", function (e) {
+            burgerMenu.classList.toggle("active");
         document.body.classList.toggle("lock");    
         responsiveMenu.classList.toggle("active");
     });   
