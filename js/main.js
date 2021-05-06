675997b9af663efcd33150354c27b346c3d45c2b
Anton <grutoha@gmail.com>
2021-05-06 14:35:29 +0300

    res

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-
+    const burgerMenu = document.querySelector(".menu__icon");
+    if (burgerMenu) {
+        const responsiveMenu = document.querySelector(".menu__body");
+        burgerMenu.addEventListener("click", function (e){
+        document.body.classList.toggle("lock");    
+        responsiveMenu.classList.toggle("active");
+    });   
+    }
 });
