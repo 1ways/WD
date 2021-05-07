6914a6ed73a6d3d088f503e6871539905832515e
Anton <grutoha@gmail.com>
2021-05-07 18:20:35 +0300

    asdasd

--- js/main.js
+++ js/main.js
@@ -1,11 +1,38 @@
-$(function(){
-    const burgerMenu = document.querySelector(".menu__icon");
-    if (burgerMenu) {
-        const responsiveMenu = document.querySelector(".menu__body");
-        burgerMenu.addEventListener("click", function (e) {
-            burgerMenu.classList.toggle("active");
-        document.body.classList.toggle("lock");    
-        responsiveMenu.classList.toggle("active");
-    });   
-    }
+$(function () {
+	const burgerMenu = document.querySelector(".menu__icon");
+	if (burgerMenu) {
+		const responsiveMenu = document.querySelector(".menu__body");
+		burgerMenu.addEventListener("click", function (e) {
+			burgerMenu.classList.toggle("active");
+			document.body.classList.toggle("lock");
+			responsiveMenu.classList.toggle("active");
+		});
+	}
+
+	const menuLinks = document.querySelectorAll(".menu__link[data-goto]");
+    if (menuLinks.length > 0) {
+        console.log(menuLinks.length);
+		menuLinks.forEach(menuLink => {
+			menuLink.addEventListener("click", OnMenuLinkClick);
+		});
+
+		function OnMenuLinkClick(e) {
+			const menuLink = e.target;
+			if (
+				menuLink.dataset.goto &&
+				document.querySelector(menuLink.dataset.goto)
+			) {
+				const gotoBlock = document.querySelector(menuLink.dataset.goto);
+				const gotoBlockValue =
+					gotoBlock.getBoundingClientRect().top +
+					pageYOffset;
+
+				window.scrollTo({
+					top: gotoBlockValue,
+					behavior: "smooth",
+                });
+                e.preventDefault();
+			}
+		}
+	}
 });
